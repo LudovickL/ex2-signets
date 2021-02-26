019360b4c26f62c8bcee4bb43b06a279e1209626
LudovickL <Luberisselud@gmail.com>
2021-02-25 19:26:36 -0500

    ajout incomplet des boutons Fab

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -1,10 +1,9 @@
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Fab from '@material-ui/core/Fab'
 import './BtnAjoutRessource.scss'
 
 export default function BtnAjoutRessource() {
   return (
-    <AddCircleIcon className="BtnAjoutRessource" style={{fontSize: 80}}>
-      Ajouter
-    </AddCircleIcon>
+  <AddCircleIcon className="BtnAjoutRessource" style={{fontSize: 80}} />
   );
 }
