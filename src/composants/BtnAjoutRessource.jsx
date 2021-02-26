c0e743020c8ebcc70815fe79ff94434f53bd41ba
LudovickL <Luberisselud@gmail.com>
2021-02-25 20:12:01 -0500

    j'ai terminé!

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -1,5 +1,4 @@
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import Fab from '@material-ui/core/Fab'
 import './BtnAjoutRessource.scss'
 
 export default function BtnAjoutRessource() {
